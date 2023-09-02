ace37c2c62a96cd9ae9087a9cf42c9e4ab35f303
Ahmed Samir <ahmed.samir@microengineering.ca>
2023-09-02 17:46:32 +0300

    update database null values

--- database/migrations/2023_07_29_125655_create_joins_table.php
+++ database/migrations/2023_07_29_125655_create_joins_table.php
@@ -18,34 +18,34 @@ return new class extends Migration
             $table->id();
 
             $table->string('name');
-            $table->enum('type',['ذكر','انثي']);
-            $table->string('nationality');
+            $table->enum('type',['ذكر','انثي'])->nullable();
+            $table->string('nationality')->nullable();
             $table->integer('age');
-            $table->string('address');
+            $table->string('address')->nullable();
             $table->integer('phone');
             $table->integer('another_phone');
-            $table->string('email');
-            $table->enum('severe_stuttering',['متوسطة','خفيفة','شديدة']);
-            $table->enum('effect_stuttering_social_life',['متوسطة','خفيفة','شديدة']);
-            $table->enum('impact_stuttering_professional_study_life',['متوسطة','خفيفة','شديدة']);
-            $table->enum('excited_overcome_stuttering',['متوسطة','خفيفة','شديدة']);
-            $table->enum('have_physical_disability',['yes','no']);
+            $table->string('email')->nullable();
+            $table->enum('severe_stuttering',['متوسطة','خفيفة','شديدة'])->nullable();
+            $table->enum('effect_stuttering_social_life',['متوسطة','خفيفة','شديدة'])->nullable();
+            $table->enum('impact_stuttering_professional_study_life',['متوسطة','خفيفة','شديدة'])->nullable();
+            $table->enum('excited_overcome_stuttering',['متوسطة','خفيفة','شديدة'])->nullable();
+            $table->enum('have_physical_disability',['yes','no'])->nullable();
             $table->string('type_disability')->nullable();
 
-            $table->enum('have_physical_mental_illness',['yes','no']);
+            $table->enum('have_physical_mental_illness',['yes','no'])->nullable();
             $table->string('type_disease')->nullable();
 
-            $table->enum('anything_related_health',['yes','no']);
+            $table->enum('anything_related_health',['yes','no'])->nullable();
             $table->string('notice')->nullable();
 
-            $table->enum('treatments_entered_club_anything_related_stuttering_before',['yes','no']);
+            $table->enum('treatments_entered_club_anything_related_stuttering_before',['yes','no'])->nullable();
             $table->string('write_down_notes_dates')->nullable();
 
-            $table->enum('anything_out_it',['yes','no']);
+            $table->enum('anything_out_it',['yes','no'])->nullable();
             $table->string('write_what_got')->nullable();
             $table->string('write_reasons_not_benefiting')->nullable();
 
-            $table->enum('how_find_out_about_us',['website','social','man','other']);
+            $table->enum('how_find_out_about_us',['website','social','man','other'])->nullable();
             $table->string('improvement_points_ideas_like_change_programs_clubs')->nullable();
 
             $table->string('admin_note')->nullable();
