9e83abf5d7f0f3e0b22ece2c766ed54ac0083074
ammaragha <ammaraghaagha@gmail.com>
2023-10-01 17:58:56 +0300

    contact us - api

---
+++ app/Http/Controllers/Api/ContactUsController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ContactUs\ContactUsRequest;
+use App\Models\ContactUs;
+use Illuminate\Http\Request;
+
+class ContactUsController extends Controller
+{
+
+
+    public function store(ContactUsRequest $request)
+    {
+        ContactUs::create($request->validated());
+        
+        // $noReplayEmail = "info@motkalem.com";
+        // Mail::to($noReplayEmail)->send(new ContactMail($request->all()));
+
+        return response()->json([
+            "message" => "Sent Successfully"
+        ]);
+    }
+}
---
+++ app/Http/Requests/ContactUs/ContactUsRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\ContactUs;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ContactUsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'], // الاسم *
+            'phone' => ['required', 'numeric'], // الهاتف *
+            'email' => ['string', 'email', 'max:255'], // البريد الالكتروني *
+            "message" => ['required', 'string']
+        ];
+    }
+}
