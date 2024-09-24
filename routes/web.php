e048c900b5834ba4f8ceb3249a04269f2f1c5330
amin-mahmoud <amin.amr.55@gmail.com>
2024-09-24 16:45:16 +0300

    modify recurring payment steps

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get(
-    'checkout-recurring/{checoutId}',
+    'checkout-recurring/{checkoutId}',
     function () {
         return view('payments.recurring-pay');
     }
