35d487c1c151565dfbb38304b0249105829ec63c
amin-mahmoud <amin.amr.55@gmail.com>
2024-09-28 13:35:09 +0300

    modify storeRecurringPaymentData action

--- app/Actions/HyperPay/ExecuteRecurringPayment.php
+++ app/Actions/HyperPay/ExecuteRecurringPayment.php
@@ -3,8 +3,6 @@
 namespace App\Actions\HyperPay;
 
 use Lorisleiva\Actions\Concerns\AsAction;
-use Illuminate\Support\Facades\Http;
-
 
 class ExecuteRecurringPayment
 {
@@ -13,22 +11,31 @@ class ExecuteRecurringPayment
     public function handle()
     {
         $registrationId = request()->registrationId;
-        $amount = 5.00;
-        $currency = 'SAR';
 
-        $url = "https://eu-prod.oppwa.com/v1/registrations/{$registrationId}/payments";
-        $data = [
-            'entityId' => env('RECURRING_ENTITY_ID'),
-            'amount' => $amount,
-            'currency' => $currency,
-            'paymentType' => 'DB',
-        ];
+        $url = "https://eu-prod.oppwa.com/v1/registrations/". $registrationId."/payments";
+        $data = "entityId=".env('RECURRING_ENTITY_ID') .
+            "&amount=5.00" .
+            "&currency=SAR" .
+            "&paymentType=DB" .
+            "&standingInstruction.mode=REPEATED" .
+            "&standingInstruction.type=UNSCHEDULED" .
+            "&standingInstruction.source=MIT";
 
-        $response = Http::withHeaders([
-            'Authorization' => 'Bearer ' .env('AUTH_TOKEN'),
-        ])->post($url, $data);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Authorization:Bearer '.env('AUTH_TOKEN')));
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);// this should be set to true in production
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $responseData = curl_exec($ch);
 
-        return response()->json($response->json());
+        if(curl_errno($ch)) {
+            return curl_error($ch);
+        }
+        curl_close($ch);
+        return json_decode($responseData);
     }
 
 }
