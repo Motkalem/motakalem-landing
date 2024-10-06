559c964bbbe0854631653e6afae2bc5983814e88
amin-mahmoud <amin.amr.55@gmail.com>
2024-10-06 14:58:36 +0300

    modify Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
 
-        $schedule->job(new CheckInstallmentPaymentsJob())->everyMinute();
+        $schedule->job(new CheckInstallmentPaymentsJob())->monthly();
     }
 
     /**
