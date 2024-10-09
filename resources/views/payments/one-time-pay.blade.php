776dc8e48761f2aac6a2ed106de123ced6e7cc26
amin-mahmoud <amin.amr.55@gmail.com>
2024-10-09 14:45:51 +0300

    last updates

--- resources/views/payments/one-time-pay.blade.php
+++ resources/views/payments/one-time-pay.blade.php
@@ -10,7 +10,6 @@
     <script src="{{env('HYPERPAY_URL')}}/paymentWidgets.js?checkoutId={{$paymentId??data_get($_GET,'checkoutId')}}"></script>
     <body>
 
-    {{env('HYPERPAY_URL')."/paymentWidgets.js?checkoutId=".$paymentId??data_get($_GET,'checkoutId') }}
         <h1 class="text-center" style="text-align: center">ستقوم بدفع مبلغ {{$payment?->package?->total . __('SAR')}}</h1>
         <form action="{{'/checkout/result/'.$_GET['pid'].'/'.$_GET['sid'].'/'}}" class="paymentWidgets" data-brands="MADA VISA MASTER"></form>
     </body>
