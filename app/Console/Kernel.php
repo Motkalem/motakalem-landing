fbef2d49d21f6a000382a7679a0275de6e16985f
amin-mahmoud <amin.amr.55@gmail.com>
2024-11-30 10:49:07 +0200

    fix login styles issue

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         Log::info('run from kernel', ['text' => 'hello from kernel']);
-        $schedule->job(new CheckInstallmentsPaymentsJob())->everyFiveMinutes();
+        $schedule->job(new CheckInstallmentsPaymentsJob())->everyThirtyMinutes();
         $schedule->job(new SendTransactionsNotificationsJob())->everyTwoMinutes();
     }
 
--- resources/views/admin/auth/auth-layout.blade.php
+++ resources/views/admin/auth/auth-layout.blade.php
@@ -10,14 +10,12 @@
     <title>تسجيل الدخول :: Motkalem</title>
 
 
-    <link rel="stylesheet" type="text/css" href="http://127.0.0.1:8000/packages/backpack/base/css/bundle.css?v=5.6.1@1b67f8efdbaa48842e0d31995068b44b8fc5c66d">
-    <link rel="stylesheet" type="text/css" href="http://127.0.0.1:8000/packages/source-sans-pro/source-sans-pro.css?v=5.6.1@1b67f8efdbaa48842e0d31995068b44b8fc5c66d">
-    <link rel="stylesheet" type="text/css" href="http://127.0.0.1:8000/packages/line-awesome/css/line-awesome.min.css?v=5.6.1@1b67f8efdbaa48842e0d31995068b44b8fc5c66d">
+    <link rel="stylesheet" type="text/css" href="{{asset('packages/backpack/base/css/bundle.css?v=5.6.1@1b67f8efdbaa48842e0d31995068b44b8fc5c66d')}}">
+    <link rel="stylesheet" type="text/css"
+          href="{{asset('packages/source-sans-pro/source-sans-pro.css?v=5.6.1@1b67f8efdbaa48842e0d31995068b44b8fc5c66d')}}">
 
-
-
-
-
+    <link rel="stylesheet" type="text/css"
+          href="{{asset('packages/line-awesome/css/line-awesome.min.css?v=5.6.1@1b67f8efdbaa48842e0d31995068b44b8fc5c66d')}}"
 
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
