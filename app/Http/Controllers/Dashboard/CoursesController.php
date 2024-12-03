68b3c834f8a98d0e3ae2c8a8e6bad2b103601ad4
amin-mahmoud <amin.amr.55@gmail.com>
2024-12-03 22:42:09 +0200

    last updates

--- app/Http/Controllers/Dashboard/CoursesController.php
+++ app/Http/Controllers/Dashboard/CoursesController.php
@@ -7,10 +7,8 @@ use App\Models\Course;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-
 
 class CoursesController extends AdminBaseController
 {
