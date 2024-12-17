399796aaae0932e66eed3cd6b3304a7c398768b6
amin-mahmoud <amin.amr.55@gmail.com>
2024-12-17 16:54:27 +0200

    add consultations sms

--- app/Http/Controllers/Dashboard/ConsultantPatientsController.php
+++ app/Http/Controllers/Dashboard/ConsultantPatientsController.php
@@ -282,7 +282,7 @@ class ConsultantPatientsController extends AdminBaseController
      */
     protected function getInvoice($id)
     {
-        $consultationPatient = ConsultantPatient::find($id);
+        $consultationPatient = ConsultantPatient::where('is_paid', 1)->find($id);
 
         return \view('payments.consultation-pay-thank-you',compact( 'consultationPatient'));
     }
