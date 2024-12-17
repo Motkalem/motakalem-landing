a21a1c54bafd5ec264d5ca8eb759aef9c2a7f0d8
amin-mahmoud <amin.amr.55@gmail.com>
2024-12-17 16:56:34 +0200

    last updates

--- app/Http/Controllers/Dashboard/ConsultantPatientsController.php
+++ app/Http/Controllers/Dashboard/ConsultantPatientsController.php
@@ -282,7 +282,7 @@ class ConsultantPatientsController extends AdminBaseController
      */
     protected function getInvoice($id)
     {
-        $consultationPatient = ConsultantPatient::where('is_paid', 1)->firstOrfail($id);
+        $consultationPatient = ConsultantPatient::where('is_paid', 1)->where('id', $id)->firstOrfail();
 
         return \view('payments.consultation-pay-thank-you',compact( 'consultationPatient'));
     }
