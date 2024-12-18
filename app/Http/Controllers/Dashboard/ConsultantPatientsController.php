147dc1c3f8c07ba500d9c94f067e02d0eb6d4e5e
Akash Haider <dev@squarement.sa>
2024-12-18 11:03:55 +0300

    change keys to motkalem sms

--- app/Http/Controllers/Dashboard/ConsultantPatientsController.php
+++ app/Http/Controllers/Dashboard/ConsultantPatientsController.php
@@ -122,7 +122,7 @@ class ConsultantPatientsController extends AdminBaseController
 
         $msg = 'عزيزي العميل، يرجى استخدام الرابط التالي لدفع تكلفة الاستشارة: ' . $paymentLink;
 
-//         (new SMS())->setPhone($consultantPatient->mobile)->SetMessage($msg)->build();
+         (new SMS())->setPhone($consultantPatient->mobile)->SetMessage($msg)->build();
 
         return redirect()->route('dashboard.consultant-patients.index')
             ->with('success',  'تم إرسال رابط الدفع بنجاح');
