060dfa6f77c8adc387236f200724a7152fc99535
amin-mahmoud <amin.amr.55@gmail.com>
2024-12-22 14:54:27 +0200

    add patient register api

--- app/Http/Controllers/Dashboard/ConsultantPatientsController.php
+++ app/Http/Controllers/Dashboard/ConsultantPatientsController.php
@@ -146,7 +146,7 @@ class ConsultantPatientsController extends AdminBaseController
 
         $msg = 'عزيزي العميل، يرجى استخدام الرابط التالي لدفع تكلفة الاستشارة: ' . $paymentLink;
 
-//         (new SMS())->setPhone($consultantPatient->mobile)->SetMessage($msg)->build();
+         (new SMS())->setPhone($consultantPatient->mobile)->SetMessage($msg)->build();
 
         return redirect()->route('dashboard.consultant-patients.index')
             ->with('success',  'تم إرسال رابط الدفع بنجاح');
@@ -303,7 +303,7 @@ class ConsultantPatientsController extends AdminBaseController
 
             $this->notifyAdmin($consultationPatient);
 
-//            (new SMS())->setPhone($consultationPatient->mobile)->SetMessage($msg)->build();
+            (new SMS())->setPhone($consultationPatient->mobile)->SetMessage($msg)->build();
 
           return $this->getInvoice($consultationPatient->id);
         } else {
@@ -436,7 +436,7 @@ class ConsultantPatientsController extends AdminBaseController
 
          $msg = 'شكرا تمت عملية الدفع : ' . $invoicetLink;
 
-//        (new SMS())->setPhone($consultationPatient->mobile)->SetMessage($msg)->build();
+        (new SMS())->setPhone($consultationPatient->mobile)->SetMessage($msg)->build();
 
         return redirect()->route('dashboard.consultant-patients.index')
             ->with('success',  'تم إرسال رابط الفاتورة بنجاح');
