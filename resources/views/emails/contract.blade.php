3aab0faf97b7e84a872c53deddf68830ffb2af8b
Your Name <amin mahmoud>
2025-01-13 17:17:46 +0200

    last updates

--- resources/views/emails/contract.blade.php
+++ resources/views/emails/contract.blade.php
@@ -143,12 +143,9 @@
                                                     3. محتوى البرنامج: تدريب على التحكم بالتأتأة.
                                                     <br/>
                                                     4. تاريخ بدء
-                                                    الدورة: {{ Carbon::parse($data->package?->starts_date)?->format('Y/n/j')  }}
+                                                    البرنامج الحضوري: {{ Carbon::parse($data->package?->starts_date)?->format('Y/n/j')  }}
                                                     .
-                                                    <br/>
-                                                    5. تاريخ انتهاء
-                                                    الدورة: {{ Carbon::parse($data->package?->ends_date)?->format('Y/n/j')  }}
-                                                    .
+
                                                 </li>
                                                 <li style="font-size:16px;font-family:Cairo,Geneva,sans-serif;color:rgb(38,38,38);padding-bottom:10px">
                                                     البند الثاني: التكلفة وجدول السداد:
