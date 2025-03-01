e037128b661444880cff5bf65d3190843a78fb35
amin-mahmoud <amin.amr.55@gmail.com>
2025-03-01 13:52:48 +0200

    modify payment layout

--- resources/views/payments/consultation-pay.blade.php
+++ resources/views/payments/consultation-pay.blade.php
@@ -168,12 +168,14 @@
                             </svg>
                         </a>
                     </div>
+
                     <div>
                         <a href="{{ url()->current() }}?{{ http_build_query(array_merge($_GET, ['brand' => 'visa'])) }}"
                            class="payment-option">
                             <img src="{{asset('images/brands/visa.png')}}" alt="Visa" />
                         </a>
                     </div>
+
                     <div>
                         <a href="{{ url()->current() }}?{{ http_build_query(array_merge($_GET, ['brand' => 'master'])) }}"
                            class="payment-option">
@@ -186,43 +188,7 @@
         @endif
     </div>
 
-    <div style="width: 100%; bottom: 0 ">
-        <footer _ngcontent-ng-c3011216936="" style="padding:0px">
-            <div _ngcontent-ng-c3011216936="" class="container">
-                <div _ngcontent-ng-c3011216936="" class="row justify-content-center align-items-center">
-                    <div _ngcontent-ng-c3011216936="" class="col-md-2 text-center">
-                        <a _ngcontent-ng-c3011216936="" href="/home">
-                            <img _ngcontent-ng-c3011216936="" src="../../../../assets/img/motkalem-new-logo.png" alt="" class="w-75">
-                        </a>
-                    </div>
-                    <div _ngcontent-ng-c3011216936="" class="col-md-12 text-center">
-                        <div _ngcontent-ng-c3011216936="" class="d-flex justify-content-around align-items-center">
-                            <a _ngcontent-ng-c3011216936="" href="https//motkalem.sa">الرئيسية</a>
-                        </div>
-                        <div _ngcontent-ng-c3011216936="" class="d-flex flex-column mt-2">
-                            <a _ngcontent-ng-c3011216936="" href="mailto:“info@motkalem.sa”" class="text-decoration-underline">info@motkalem.sa</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </footer>
-        <section class="first_footer py-3" style="background: #34474f ; padding:0px">
-            <div _ngcontent-ng-c3011216936="" class="container-fluid align-self-center"  >
-                <div class="row justify-content-center  mb-0" style=" ">
-
-                    <div  class="col-md-4 text-center align-self-center mt-1 border-left"  >
-                        <p  class="text-white"  style=" line-height: unset;font-size: 12px"> رقم السجل التجاري : 4030511477 </p>
-                    </div>
-                    <div  class="col-md-4 text-center align-self-center mt-1 border-left" >
-                        <p   class="text-white" style="   line-height: unset;font-size: 12px">  رقم التسجيل الضريبي : 312011490100003
-                        </p>
-                    </div>
-
-                </div>
-            </div>
-        </section>
-    </div>
+    @include('payments._inc.footer')
 </div>
 <script>
     function removeBrandParam() {
