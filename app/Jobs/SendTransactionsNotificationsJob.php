ac7d1dc2ac7aeda2570c743475a8c0f3f85a692b
Akash Haider <dev@squarement.sa>
2025-10-01 17:58:09 +0300

    debug

--- app/Jobs/SendTransactionsNotificationsJob.php
+++ app/Jobs/SendTransactionsNotificationsJob.php
@@ -44,9 +44,11 @@ class SendTransactionsNotificationsJob   implements ShouldQueue
                 $query->where('admin_notified', 0)
                     ->orWhere('student_notified', 0);
             })->get();
+        Log::notice('Found ' . $installmentNotifications->count() . ' notifications');
 
 
         foreach ($installmentNotifications as $notification) {
+            Log::notice('Processing notification ID: ' . $notification->id . ', amount: ' . $notification->amount);
 
             if ( $notification->amount != 0 ) {
 
