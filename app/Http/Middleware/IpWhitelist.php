7fcfee9f30fd23c5a6d019523bef6c4bb3f8b9d4
Akash Haider <akash_mangi@hotmail.com>
2025-10-19 20:20:11 +0300

    fix

--- app/Http/Middleware/IpWhitelist.php
+++ app/Http/Middleware/IpWhitelist.php
@@ -10,7 +10,7 @@ class IpWhitelist
     public function handle(Request $request, Closure $next)
     {
         // Get allowed IPs from .env
-        $whitelist = explode(',', env('ALLOWED_API_IPS', ''));
+        /*$whitelist = explode(',', env('ALLOWED_API_IPS', ''));
 
         // Always allow requests from the server itself
         $serverIps = ['127.0.0.1', '::1', $_SERVER['SERVER_ADDR'] ?? ''];
@@ -19,7 +19,7 @@ class IpWhitelist
 
         if (!in_array($requestIp, $whitelist) && !in_array($requestIp, $serverIps)) {
             return response()->json(['message' => 'Unauthorized'], 403);
-        }
+        }*/
 
         return $next($request);
     }
